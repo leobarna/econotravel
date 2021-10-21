3a59f19de4457069d4898924aee2acc1488f346c
Leo <82203547+leobarna@users.noreply.github.com>
2021-10-21 23:24:55 +0200

    create model, repository restcontroller

---
+++ src/main/java/com/example/econotravel/EconotravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.econotravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconotravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EconotravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/econotravel/PruebaeconoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.econotravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaeconoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
