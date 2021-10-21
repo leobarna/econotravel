fb546cc3fb0d687f67cc721ef236c4e32aa31d0a
leobarna <leonoradli94@gmail.com>
2021-10-21 23:18:30 +0200

    create model, repository, restcontroller

---
+++ src/main/java/com/example/econotravel/controller/ExperienceController.java
@@ -0,0 +1,23 @@
+package com.example.econotravel.controller;
+
+import com.example.econotravel.repository.ExperienceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ExperienceController {
+    private ExperienceRepository experienceRepository;
+
+    @Autowired
+//constructor
+    public ExperienceController(ExperienceRepository experienceRepository) {
+        this.experienceRepository = experienceRepository;
+    }
+    @GetMapping("/experiences")
+    public List<Experience> allExperiences() {
+
+        return experienceRepository.findAll();
+    }}
---
+++ src/main/java/com/example/econotravel/model/Experience.java
@@ -0,0 +1,52 @@
+package com.example.econotravel.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table
+public class Experience implements Serializable {
+    //atribute id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String  name;
+    private BigDecimal pricePerPerson;
+
+    //constructor
+    public Experience(String name, BigDecimal pricePerPerson) {
+        this.name = name;
+        this.pricePerPerson = pricePerPerson;
+    }
+    //Class 'Experience' should have [public, protected] no-arg constructor
+    public Experience() {
+
+    }
+
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPricePerPerson() {
+        return pricePerPerson;
+    }
+
+    public void setPricePerPerson(BigDecimal pricePerPerson) {
+        this.pricePerPerson = pricePerPerson;
+    }
+}
+
+
